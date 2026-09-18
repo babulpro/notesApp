833176cf5a29a801e43abb99c5aa3e73804fabc2
babulpro <sh8657706@gmail.com>
2026-09-18 09:26:11 +0600

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <div>
+        <h2>Notes app</h2>
+        An example app for {" "}<a href={"/"}>Full Stack Open Next.js</a>
+      </div>
+      <div>
+        <h1>See</h1>
+        <a href='https://github.com/babulpro/notesApp'>Github</a>
+      </div>
+      
+    </div>
+  );
+};
+
+export default HomePage;
